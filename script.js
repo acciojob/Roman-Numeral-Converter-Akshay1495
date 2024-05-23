a83734832cf5c5f5c94bdd1c37b8a5ff508057cc
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-23 08:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let roman = '';
-  const keys = Object.keys(obj).map(Number).sort((a, b) => b - a);
 
-  for (let i = 0; i < keys.length; i++) {
-    let [letter, value] = obj[keys[i]];
+  for (let [letter, value] of romanNumerals) {
     while (num >= value) {
       roman += letter;
       num -= value;
-    }
-
-    if (i % 2 === 0 && i < keys.length - 1) {
-      let [nextLetter, nextValue] = obj[keys[i + 2]];
-      if (num >= value - nextValue) {
-        roman += nextLetter + letter;
-        num -= (value - nextValue);
-      }
-    } else if (i % 2 !== 0 && i < keys.length - 2) {
-      let [nextLetter, nextValue] = obj[keys[i + 2]];
-      if (num >= value - nextValue) {
-        roman += nextLetter + letter;
-        num -= (value - nextValue);
-      }
     }
   }
 
   return roman;
 }
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
